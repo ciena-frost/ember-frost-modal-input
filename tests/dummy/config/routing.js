3c672dd6f3b6f6d02741539d205afcfb88b53d34
Lisa Irwin <lisairwin4@gmail.com>
2016-05-06 17:09:36 -0700

    create routes on dummy app for default, perfect-scroll modals

--- tests/dummy/config/routing.js
+++ tests/dummy/config/routing.js
@@ -2,18 +2,43 @@ module.exports = [
   {
     id: 'demo',
     alias: 'Demo',
-    type: 'route',
+    type: 'category',
     route: 'demo',
     path: {
       path: '/'
     },
-    modalName: 'frost-create-form',
-    modal: {
-      withParams: 'showModalForm',
-      dialogClass: 'frost-bunsen-form-dialog',
-      actions: {
-        save: 'save'
+    items: [{
+      id: 'default',
+      alias: 'Default',
+      type: 'route',
+      route: 'demo.default',
+      path: {
+        path: '/default'
+      },
+      modalName: 'frost-create-form',
+      modal: {
+        withParams: 'showModalForm',
+        dialogClass: 'frost-bunsen-form-dialog',
+        actions: {
+          save: 'save'
+        }
       }
-    }
+    }, {
+      id: 'perfect-scroll',
+      alias: 'Perfect Scroll',
+      type: 'route',
+      route: 'demo.perfect-scroll',
+      path: {
+        path: '/perfect-scroll'
+      },
+      modalName: 'frost-create-form-with-scroll',
+      modal: {
+        withParams: 'showModalForm',
+        dialogClass: 'frost-bunsen-form-dialog',
+        actions: {
+          save: 'save'
+        }
+      }
+    }]
   }
 ]
