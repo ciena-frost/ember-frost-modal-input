8263d5851874d5d90a76f950a8f6fde79406d239
Adam Meadows <adam.meadows@gmail.com>
2016-06-30 15:47:33 -0600

    Cleanup sass nesting

--- blueprints/ember-frost-modal-input/index.js
+++ blueprints/ember-frost-modal-input/index.js
@@ -12,7 +12,8 @@ module.exports = {
     return this.addAddonsToProject({
       packages: [
         {name: 'ember-frost-core', target: '>=0.8.0 <2.0.0'},
-        {name: 'ember-frost-modal', target: '^2.0.0'},
+        {name: 'ember-frost-bunsen', target: '^6.4.0'},
+        {name: 'ember-frost-modal', target: '^2.2.1'},
         {name: 'ember-block-slots', target: '^0.12.2'},
         {name: 'ember-prop-types', target: '~2.0.0'},
         {name: 'ember-wormhole', target: '0.3.5'}
